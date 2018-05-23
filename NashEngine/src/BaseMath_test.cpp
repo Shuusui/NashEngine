809709bb560bd25bb204d5f69db22cf17ff2dc98
Unknown <K.schaefer92@gmx.de>
2018-05-23 11:18:36 +0200

    added new unit tests

--- NashEngine/src/BaseMath_test.cpp
+++ NashEngine/src/BaseMath_test.cpp
@@ -7,4 +7,29 @@ TEST(basemath, AbsF)
 	float x = -1.5f;
 	x = Nash::AbsF(x);
 	EXPECT_FLOAT_EQ(x, 1.5f);
+	x = Nash::AbsF(x);
+	EXPECT_FLOAT_EQ(x, 1.5f);
 }
+
+TEST(basmath, AbsD)
+{
+	double x = -2.5f; 
+	x = Nash::AbsD(x);
+	EXPECT_FLOAT_EQ(x, 2.5f);
+	x = Nash::AbsD(x);
+	EXPECT_FLOAT_EQ(x, 2.5f);
+}
+
+TEST(baseMath, CompareF)
+{
+
+	EXPECT_TRUE(Nash::Compare(1.511232553123123f, 1.511232553112351f));
+	EXPECT_FALSE(Nash::Compare(1.5112324f, 1.5112325f));
+	//TODO: Add test for CompareD
+}
+TEST(baseMath, SqrtF)
+{
+	float x = 9; 
+	EXPECT_FLOAT_EQ(3.0f, Nash::Sqrt(x));
+}
+
